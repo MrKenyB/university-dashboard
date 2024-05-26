1740182339a9bc8e072a2bc22129f89a474bfcce
MrKenyB <kenrepangou@gmail.com>
2024-05-26 23:41:19 +0200

    Mise à jour du Dashboard avec la fonctionnalité de détails des étudiants

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Typography, Button } from '@mui/material';
+import './Home.css'; // Import du fichier CSS
 
 const Home = () => {
   return (
-    <Container>
-      <Typography variant="h2" gutterBottom>Bienvenue sur le Tableau de Bord de l'Université DGC</Typography>
-      <Button variant="contained" color="primary">
-        <Link to="/dashboard" style={{ color: 'white', textDecoration: 'none' }}>
-          Aller au Tableau de Bord
-        </Link>
-      </Button>
-    </Container>
+    <div className="body-background">
+      <div className="background"></div> {/* Ajouter l'élément d'arrière-plan flou */}
+      <Container className="container">
+        <Typography variant="h2" gutterBottom>Bienvenue sur le Tableau de Bord de l'Université DGC</Typography>
+        <Button variant="contained" color="primary">
+          <Link to="/dashboard" style={{ color: 'white', textDecoration: 'none' }}>
+            Aller au Tableau de Bord
+          </Link>
+        </Button>
+      </Container>
+    </div>
   );
 };
 
