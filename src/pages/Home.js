1a681f26d365f97c401e2e6897f47807d51e9fa3
MrKenyB <kenrepangou@gmail.com>
2024-06-02 03:03:06 +0200

    Mise à jour du Dashboard avec la fonctionnalité de détails des étudiants

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,37 @@
-// src/pages/Home.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Button, CircularProgress } from '@mui/material';
 import './Home.css'; // Import du fichier CSS
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDashboardTransition = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Mettez le délai souhaité ici (en millisecondes)
+  };
+
   return (
     <div className="body-background">
       <div className="background"></div> {/* Ajouter l'élément d'arrière-plan flou */}
       <Container className="container">
-        <Typography variant="h2" gutterBottom>Bienvenue sur le Tableau de Bord de l'Université DGC</Typography>
-        <Button variant="contained" color="primary">
-          <Link to="/dashboard" style={{ color: 'white', textDecoration: 'none' }}>
-            Aller au Tableau de Bord
-          </Link>
-        </Button>
+        <Typography variant="h2" gutterBottom className="title">
+          Bienvenue sur le Tableau de Bord de l'Université DGC
+        </Typography>
+        {/* Bouton pour passer à la page du tableau de bord */}
+        {!loading ? (
+          <Button variant="contained" color="primary" className="dashboard-button" onClick={handleDashboardTransition}>
+            <Link to="/dashboard" className="link">
+              Aller au Tableau de Bord
+            </Link>
+          </Button>
+        ) : (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <CircularProgress color="primary" />
+          </div>
+        )}
       </Container>
     </div>
   );
